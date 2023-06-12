86c565060daa5ae7e38b9a40f2c2867039ed23de
harsh-goyal-01 <77243780+harsh-goyal-01@users.noreply.github.com>
2023-06-12 15:33:46 +0530

    Add files via upload

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = function() {
+    return{
+        webpack: {
+  plugins: [
+    new BundleAnalyzerPlugin({analyzerMode: "server"})
+  ],
+},
+    };
+};
